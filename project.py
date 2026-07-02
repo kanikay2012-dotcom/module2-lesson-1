4789f16d2e6d48b307ce62316409d54fa663b765
kanikay2012-dotcom <kanika.y2012@gmail.com>
2026-07-02 17:35:49 +0530

    project.py

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 age = int(input("Enter your age:"))
-if age ==10 or age>10 or age ==20 or age<20:
+if age ==10 or age>10 :
     print("The user is allowed to use this application.")
+    if age ==20 or age<20:
 else:
     print("You are not allowed to use this application.")
